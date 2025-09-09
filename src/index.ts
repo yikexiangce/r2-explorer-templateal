650d5aee60595b71830d90de0e47e995ecd7c0cf
一刻相册 <china@aliyun.com>
2025-09-10 06:45:03 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -7,4 +7,9 @@ export default R2Explorer({
   // Learn more how to secure your R2 Explorer instance:
   // https://r2explorer.com/getting-started/security/
   // cfAccessTeamName: "my-team-name",
-});
+
+  export default R2Explorer({ readonly: false, basicAuth: {
+    username: 'anotherUser',
+    password: '123567'
+  }
+})
